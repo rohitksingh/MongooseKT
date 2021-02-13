cb51daf8bc99280c8a08a0f6f40bb8742c3badc0
Rohit <r4rohit002@gmail.com>
2021-02-13 11:41:37 -0700

    Initial commit

---
+++ app/src/main/java/com/rohitksingh/mongoosekt/ShowListActivity.kt
@@ -0,0 +1,12 @@
+package com.rohitksingh.mongoosekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
